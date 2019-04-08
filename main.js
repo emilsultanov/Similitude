ed2366ae9a780ee46e1d1eee83d6384d4a201474
Emil <emilsultanov01@gmail.com>
2019-04-08 20:54:15 +0400

    the best

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
 function similitude(a,b){
 
-			let aClean=a.replace(/(.)(?=.*\1)/g, "");
-			let bClean=b.replace(/(.)(?=.*\1)/g, "");
-			let k;
-			if(aClean.length>bClean.length){
-				k=Math.floor((bClean.length/aClean.length)*100);
-				return k;
-			}
-			else if(aClean.length<bClean.length){
-				k=Math.floor((aClean.length/bClean.length)*100);
-				return k;
-			}
-			else{
-				let merge =aClean.concat(bClean);
-				let mergeClean=merge.replace(/(.)(?=.*\1)/g, "");
-				k=Math.floor(((merge.length-mergeClean.length)/aClean.length)*100);
-				return k;
-			}
-		};
+	let aFilter=a.replace(/(.)(?=.*\1)/g, "");
+	let bfilter=b.replace(/(.)(?=.*\1)/g, "");
+	
+	let arr1=aFilter.split('');
+	let arr2=bfilter.split('');
+	
+	let percent,k=0;
+
+	if(arr1.length>arr2.length){
+		arr2.forEach((i)=>{
+			arr1.forEach((j)=>{
+				if(i===j){
+					k+=1;
+				}
+			});
+		});
+		percent=Math.floor(k/arr1.length*100);
+		return percent;
+	}
+
+	else if(arr1.length<arr2.length){
+		arr1.forEach((i)=>{
+			arr2.forEach((j)=>{
+				if(i===j){
+					k+=1;
+				}
+			});
+		});
+		percent=Math.floor(k/arr2.length*100);
+		return percent;
+	}
+
+	else{
+		arr1.forEach((i)=>{
+			arr2.forEach((j)=>{
+				if(i===j){
+					k+=1;
+				}
+			});
+		});
+		percent=Math.floor(k/arr2.length*100);
+		return percent;
+	}
+};
